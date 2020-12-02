b2a1c995a5ffbef1259d1625ffb25f9fc60dd8ee
sumonsheikh <sm.sumon117@gmail.com>
2020-12-02 18:39:08 +0600

    create list component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import List from './List';
+
+function App() {
+  return (
+    <main>
+      <div className='container'>
+        <h3> 0 Birthday Today</h3>
+        <List/>
+        <button>Clear All</button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/List.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const List = () => {
+    return (
+        <div>
+            <h2>List Componet</h2>
+        </div>
+    )
+}
+export default List;
